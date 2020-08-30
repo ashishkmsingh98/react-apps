0945cfca7a0e5dce514faf0aba644f0043b69606
ashishkmsingh98 <ashishkmsingh98@gmail.com>
2020-08-30 16:17:27 +0530

    Updates

---
+++ precache-manifest.c03a84d188132808c3c0b1fbb077a7f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d1404053780c28b45f7f00beaec7fc",
+    "url": "/react-apps/index.html"
+  },
+  {
+    "revision": "253ccff375f1d3caf9ea",
+    "url": "/react-apps/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "813f953973304d132bd6",
+    "url": "/react-apps/static/css/main.6a61b3fa.chunk.css"
+  },
+  {
+    "revision": "253ccff375f1d3caf9ea",
+    "url": "/react-apps/static/js/2.991dd4ce.chunk.js"
+  },
+  {
+    "revision": "813f953973304d132bd6",
+    "url": "/react-apps/static/js/main.32ded309.chunk.js"
+  },
+  {
+    "revision": "3a5d06dbfaa8382bedf9",
+    "url": "/react-apps/static/js/runtime-main.f89b3141.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-apps/static/media/bg.b6796c73.jpg"
+  }
+]);
